47a4bc1281eb93d340bae7b17d51817328270816
kimmi34 <49889503+kimmi34@users.noreply.github.com>
2020-11-22 21:59:45 +0530

    Update node_server.py

--- node_server.py
+++ node_server.py
@@ -15,9 +15,6 @@ class Block:
         self.nonce = nonce
 
     def compute_hash(self):
-        """
-        A function that return the hash of the block contents.
-        """
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
 
@@ -31,11 +28,6 @@ class Blockchain:
         self.chain = []
 
     def create_genesis_block(self):
-        """
-        A function to generate genesis block and appends it to
-        the chain. The block has index 0, previous_hash as 0, and
-        a valid hash.
-        """
         genesis_block = Block(0, [], 0, "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
@@ -45,13 +37,6 @@ class Blockchain:
         return self.chain[-1]
 
     def add_block(self, block, proof):
-        """
-        A function that adds the block to the chain after verification.
-        Verification includes:
-        * Checking if the proof is valid.
-        * The previous_hash referred in the block and the hash of latest block
-          in the chain match.
-        """
         previous_hash = self.last_block.hash
 
         if previous_hash != block.previous_hash:
@@ -66,10 +51,6 @@ class Blockchain:
 
     @staticmethod
     def proof_of_work(block):
-        """
-        Function that tries different values of nonce to get a hash
-        that satisfies our difficulty criteria.
-        """
         block.nonce = 0
 
         computed_hash = block.compute_hash()
@@ -84,10 +65,6 @@ class Blockchain:
 
     @classmethod
     def is_valid_proof(cls, block, block_hash):
-        """
-        Check if block_hash is valid hash of block and satisfies
-        the difficulty criteria.
-        """
         return (block_hash.startswith('0' * Blockchain.difficulty) and
                 block_hash == block.compute_hash())
 
@@ -98,8 +75,6 @@ class Blockchain:
 
         for block in chain:
             block_hash = block.hash
-            # remove the hash field to recompute the hash again
-            # using `compute_hash` method.
             delattr(block, "hash")
 
             if not cls.is_valid_proof(block, block_hash) or \
@@ -112,11 +87,6 @@ class Blockchain:
         return result
 
     def mine(self):
-        """
-        This function serves as an interface to add the pending
-        transactions to the blockchain by adding them to the block
-        and figuring out Proof Of Work.
-        """
         if not self.unconfirmed_transactions:
             return False
 
@@ -286,10 +256,6 @@ def get_pending_tx():
 
 
 def consensus():
-    """
-    Our naive consnsus algorithm. If a longer valid chain is
-    found, our chain is replaced with it.
-    """
     global blockchain
 
     longest_chain = None
@@ -323,5 +289,4 @@ def announce_new_block(block):
                       data=json.dumps(block.__dict__, sort_keys=True),
                       headers=headers)
 
-# Uncomment this line if you want to specify the port number in the code
-#app.run(debug=True, port=8000)
+
